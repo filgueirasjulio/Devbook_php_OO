aea0db940825116440ec559df78e6c9f516fc584
filgueirasjulio <julio_filgueiras@live.com>
2022-04-09 13:16:51 -0300

    fix(PDO): acentuação ao salvar registros

--- config.php
+++ config.php
@@ -9,4 +9,4 @@ $db_host = 'localhost';
 $db_user = 'root';
 $db_pass = '';
 
-$pdo = new PDO("mysql:dbname=".$db_name.";host=".$db_host, $db_user, $db_pass);
+$pdo = new PDO("mysql:dbname=".$db_name.";host=".$db_host, $db_user, $db_pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", PDO::ATTR_EMULATE_PREPARES   => false));
